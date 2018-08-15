4f824816c175c823d861460fb16fec754adb7d76
Leon <l.braun@lambus.io>
2018-08-16 01:09:32 +0200

    add first functions
    addPCF, init, lcd_clear, lcd_fire working

--- PCF8576.cpp
+++ PCF8576.cpp
@@ -1,2 +1,60 @@
 #include "Arduino.h"
 #include "PCF8576.h"
+#include <Wire.h>
+
+
+//Constructor, initialize Wire lib
+PCF8576::PCF8576()
+{
+    Wire.begin();
+}
+
+//Begin method
+void PCF8576::begin(){
+    
+}
+
+void PCF8576::addPCF(uint8_t pcfAddr, uint8_t modeSet, uint8_t devSel, uint8_t blink, uint8_t bankSel){
+    settings[PCFcount][0] = pcfAddr;
+    settings[PCFcount][1] = modeSet;
+    settings[PCFcount][2] = devSel;
+    settings[PCFcount][3] = blink;
+    settings[PCFcount][4] = bankSel;
+    PCFcount++;
+}
+
+void PCF8576::init(){
+    //init all added PCF's, don't know exactly if you have to init all
+    for (uint8_t pcf = 0; pcf < PCFcount; pcf++){
+        Serial.println("init");
+        Serial.println(settings[pcf][0]);
+        Wire.beginTransmission(settings[pcf][0]);
+        Wire.write(CONTINUE | settings[pcf][1]); //modeset
+        Wire.write(CONTINUE | settings[pcf][3]); //blink
+        Wire.write(LAST | settings[pcf][2]); //devSel
+        //Wire.write(LAST | settings[pcf][4]); //banksel
+        Wire.endTransmission();
+    }
+}
+
+void PCF8576::lcd_clear(){
+    //works only for one Hardware Adress for now, have to implement the logic for two
+    Wire.beginTransmission(settings[0][0]);
+	Wire.write(CONTINUE | DEVICE_SELECT); //select the first PCF, assume that the first is the device with 000, maybe implement another logic
+	Wire.write(LOADDATAPOINTER);
+	for (int i=0; i < PCFcount * 20; i++) {
+		Wire.write(0x00);
+	}
+	Wire.endTransmission();
+}
+
+void PCF8576::lcd_fire(){
+    //same as in lcd_clear()
+    Wire.beginTransmission(settings[0][0]);
+	Wire.write(CONTINUE | DEVICE_SELECT); //select the first PCF, assume that the first is the device with 000, maybe implement another logic
+	Wire.write(LOADDATAPOINTER);
+	for (int i=0; i < PCFcount * 20; i++) {
+		Wire.write(0xFF);
+	}
+	Wire.endTransmission();
+}
--- PCF8576.h
+++ PCF8576.h
@@ -3,4 +3,74 @@
 
 #include "Arduino.h"
 
+//Continue and Last Bit
+#define CONTINUE 128			// Command continuation Bit
+#define LAST 0					// Last Command
+
+//MODE SET
+#define MODESET 64				// Mode Set Command
+
+#define MODE_NORMAL 0			// Display Mode
+#define MODE_POWERSAVING 16
+
+#define DISPLAY_DISABLED 0		// Display On/Off
+#define DISPLAY_ENABLED 8
+
+#define BIAS_THIRD 0			// BIAS Mode
+#define BIAS_HALF 4
+
+#define DRIVE_STATIC 1			// Drive Mode
+#define DRIVE_2 2
+#define DRIVE_3 3
+#define DRIVE_4 0
+
+
+//BLINK
+#define BLINK  112				// Blink Command
+
+#define BLINKING_NORMAL 0		// Blink Mode
+#define BLINKING_ALTERNATION 4
+
+#define BLINK_FREQUENCY_OFF 0	// Blink Frequency
+#define BLINK_FREQUENCY2 1
+#define BLINK_FREQUENCY1 2
+#define BLINK_FREQUENCY05 3
+
+
+//LOADDATAPOINTER
+#define LOADDATAPOINTER  0x00	// Data pointer start address
+
+
+//BANK SELECT
+#define BANKSELECT 120			// Bank Select Command
+
+#define BANKSELECT_O1_RAM0 0
+#define BANKSELECT_O1_RAM2 2
+
+#define BANKSELECT_O2_RAM0 0
+#define BANKSELECT_O2_RAM2 1
+
+
+// Device Select, not really sure how to implement best, 
+//because you can combine these commands to address 8 PCF
+#define DEVICE_SELECT 96		// Device Select Command
+#define DEVICE_SELECT_A2 4
+#define DEVICE_SELECT_A1 2
+#define DEVICE_SELECT_A0 1		// A2,A1,A0 = B111
+
+class PCF8576
+{
+    public:
+        PCF8576();
+        void begin();
+        void addPCF(uint8_t pcfAddr, uint8_t modeSet, uint8_t devSel, uint8_t blink, uint8_t bankSel);
+        void init();
+        void lcd_clear();
+        void lcd_fire();
+    private:
+        uint8_t settings[16][5];
+        uint8_t PCFcount = 0;
+
+};
+
 #endif
