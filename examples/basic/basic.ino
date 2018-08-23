0b6e84c42375e4aed42fc335d5fa2023f9531510
Leon <l.braun@lambus.io>
2018-08-23 13:58:47 +0200

    update bias and name conversion

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,11 +1,11 @@
 #include <LX_Lcd_Vario.h>
 
 //create the object
-LX_Lcd_Vario pcf;
+LX_Lcd_Vario lx;
 
 //Setup the Variables that you have to pass to the library, this works for the LX Vario
 #define PCFAddress B111000 //This defines the Slave Adress on the I2C Bus, its controlled via the SA0 Pin
-byte set_modeset = MODESET | MODE_NORMAL | DISPLAY_ENABLED | BIAS_HALF | DRIVE_4;
+byte set_modeset = MODESET | MODE_NORMAL | DISPLAY_ENABLED | BIAS_THIRD | DRIVE_4;
 byte set_blink = BLINK | BLINKING_ALTERNATION | BLINK_FREQUENCY_OFF;
 byte set_datapointer = LOADDATAPOINTER | 0;
 byte set_bankselect = BANKSELECT | BANKSELECT_O1_RAM0 | BANKSELECT_O2_RAM0; //doesn't really matter, because Drive mode is 4
@@ -19,51 +19,51 @@ void setup() {
   digitalWrite(7, HIGH);
   delay(100); //time to wake up
 	//add your controller to the Library.
-	pcf.addPCF(B111000, set_modeset, set_deviceselect_1, set_blink, set_bankselect);
-	pcf.addPCF(B111000, set_modeset, set_deviceselect_2, set_blink, set_bankselect);
+	lx.addPCF(B111000, set_modeset, set_deviceselect_1, set_blink, set_bankselect);
+	lx.addPCF(B111000, set_modeset, set_deviceselect_2, set_blink, set_bankselect);
 	//setup the controller
-	pcf.init();
+	lx.init();
 }
 
 void loop(){
 	//sets all on
-	pcf.fire();
+	lx.fire();
 	delay(1000);
 	
 	//clear the display
-	pcf.clear();
+	lx.clear();
 	delay(1000);
 
 	//Let the indicator go up
 	for (float i = -5; i <= 5; i = i + 0.1){
 		//the function takes values in m/s
-		pcf.addInd(i);
+		lx.addInd(i);
 		//Every function, except clear() and fire(), add their display data to a buffer. You have to call show() to bring it to the display
-		pcf.show();
+		lx.show();
 		delay(100);
 	}
 
 	//Let the Speed Command Ring go up
 	for (int i = 0; i < 13; i++){
 		//this function takes only numbers of the indicators, for now
-		pcf.addScr(i);
-		pcf.show();
+		lx.addScr(i);
+		lx.show();
 		delay(100);
 	}
 
 	//Write numbers
 	for (int i = 1; i < 10; i++){
 		//this function writes a number to the selected position, see more in readme
-		pcf.addNumber(i, i);
-		pcf.show();
+		lx.addNumber(i, i);
+		lx.show();
 		delay(100);
 	}
 
 	//Show symbols
 	for (int i = 0; i < 22; i++){
 		//this function shows the symbols, for now you have to look them up in the mapping.
-		pcf.addSym(i);
-		pcf.show();
+		lx.addSym(i);
+		lx.show();
 		delay(100);
 	}
 }
